cacc01267430af51853ac5cbb87fc66691156ed1
zayden <riverzayden@gmail.com>
2019-12-07 23:35:08 +0900

    3_myapp Model 설정 및 admin확인

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Shop, Product
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['거래처코드', '거래처명', '내용']
+    list_display_links = ['거래처코드']
+    ordering = ('거래처코드',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['품목코드', '품목명', '거래처코드']
+    list_display_links = ['품목코드']
+    ordering = ('품목코드',)
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from ecount.utils import uuid_upload_to
 
-# Create your models here.
+
+class Shop(models.Model):
+    거래처코드 = models.CharField(max_length=10, primary_key=True)
+    거래처명 = models.CharField(max_length=20)
+    내용 = models.TextField()
+    생성시간 = models.DateTimeField(auto_now_add=True)
+    수정시간 = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.거래처코드
+
+class Product(models.Model):
+    품목코드= models.CharField(max_length=20)
+    품목명= models.CharField(max_length=20)
+    품목구분= models.CharField(max_length=20)
+    거래처코드 = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    색상= models.CharField(max_length=20, blank=True)
+    단위= models.CharField(max_length=20, blank=True)
+    개수 = models.IntegerField()
+    입고단가 = models.IntegerField()
+    이미지 = models.ImageField( blank=True, upload_to=uuid_upload_to)
+    내용 = models.TextField(blank=True)
+    생성시간 = models.DateTimeField(auto_now_add=True)
+    수정시간 = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.품목코드
